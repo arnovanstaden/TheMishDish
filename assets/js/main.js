b211881facad6296b096063da405cefecbed8855
Arno van Staden <arnovanstaden@gmail.com>
2020-01-17 10:17:18 +0200

    update recipe page + menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,10 +38,49 @@ $(document).ready(function () {
   });
 });
 
-$(".next-image").click(function() {
-  $(".recipe-image").css("background-image",'url("/assets/images/recipes/M001/2.jpg")')
+$(".next-image").click(function () {
+  $(".recipe-image").css("background-image", 'url("/assets/images/recipes/M001/2.jpg")')
 });
 
-$(".prev-image").click(function() {
-  $(".recipe-image").css("background-image",'url("/assets/images/recipes/M001/1.jpg")')
+$(".prev-image").click(function () {
+  $(".recipe-image").css("background-image", 'url("/assets/images/recipes/M001/1.jpg")')
 });
+
+
+// Menu Icon
+
+function openMenu() {
+
+  if ($(".navbar-content").hasClass("navbar-open")) {
+
+
+
+    // Toggle Icon Change
+    $(".navbar-icon").toggleClass("change");
+
+    // Close Menu
+    $(".navbar-content").css("width", "0");
+    $(".navbar-content").css("opacity", "0");
+
+    // Enable Scroll
+    $("body").css("overflow-y", "scroll");
+
+    // Toggle Navbar Open Class
+    $(".navbar-content").toggleClass("navbar-open")
+
+  } else {
+
+    // Open Menu
+    $(".navbar-content").css("width", "60%");
+    $(".navbar-content").css("opacity", "1");
+
+    // Toggle Icon Change
+    $(".navbar-icon").toggleClass("change");
+
+    // Disable Scroll
+    $("body").css("overflow-y", "hidden");
+
+    // Toggle Navbar Open Class
+    $(".navbar-content").toggleClass("navbar-open");
+  }
+}
